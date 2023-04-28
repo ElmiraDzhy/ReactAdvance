b71a32fd43e6b4d2e51d71a702142eb56cee82e5
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-28 11:16:49 +0300

    Add switch mui

--- src/components/Tree/Parent/SubParent/Child/InnerChild/index.jsx
+++ src/components/Tree/Parent/SubParent/Child/InnerChild/index.jsx
@@ -3,32 +3,39 @@ import { UserContext, ThemeContext } from '../../../../../../contexts';
 import classNames from 'classnames';
 import styles from './Child.module.css';
 import { CONSTANTS } from '../../../../../../constants';
+import { Switch } from '@mui/material';
 
 function InnerChild() {
   return (
     <ThemeContext.Consumer>
       {([theme, themeChange]) => {
-        return <UserContext.Consumer>
-          {([user, logOut]) => {
-            const className = classNames({
-              [styles.light]: theme === CONSTANTS.THEMES.LIGHT,
-              [styles.dark]: theme === CONSTANTS.THEMES.DARK,
-            });
-            return (
-              <div className={className}>
-                <img
-                  style={{ width: '300px', height: '300px' }}
-                  src={user.avatar}
-                  alt=""
-                />
-                <p>{user.name}</p>
-                <p>{user.surname}</p>
-                <button onClick={ logOut }>LogOut</button>
-                <button onClick={themeChange}>theme</button>
-              </div>
-            );
-          }}
-        </UserContext.Consumer>;
+        return (
+          <UserContext.Consumer>
+            {([user, logOut]) => {
+              const className = classNames({
+                [styles.light]: theme === CONSTANTS.THEMES.LIGHT,
+                [styles.dark]: theme === CONSTANTS.THEMES.DARK,
+              });
+              return (
+                <div className={className}>
+                  <img
+                    style={{ width: '300px', height: '300px' }}
+                    src={user.avatar}
+                    alt=""
+                  />
+                  <p>{user.name}</p>
+                  <p>{user.surname}</p>
+                  <button onClick={logOut}>LogOut</button>
+                  <Switch
+                    checked={theme === CONSTANTS.THEMES.LIGHT}
+                    onChange={themeChange}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                  />
+                </div>
+              );
+            }}
+          </UserContext.Consumer>
+        );
       }}
     </ThemeContext.Consumer>
   );
