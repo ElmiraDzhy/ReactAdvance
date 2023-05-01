2fbfc32d7eec3cdd3fa6fe5a63ee1956a03b70a5
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-01 11:26:25 +0300

    Rewrite form into Formik

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './SignInForm.module.css';
 import * as yup from 'yup';
+import { Field, Form, Formik } from 'formik';
 
 const SIGN_UP_SCHEMA = yup.object({
   email: yup.string().required(),
@@ -13,72 +14,36 @@ const SIGN_UP_SCHEMA = yup.object({
   lastName: yup.string(),
 });
 
-export default class SignUpForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-      error: null,
-    };
-  }
-
-  changeHandler = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+const SignUpForm = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
   };
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, action) => {
+        console.log(values);
+        console.log(action);
+        action.resetForm();
+      }}
+    >
+      {formikProps => {
+        return (
+          <Form className={styles.form}>
+            <Field name="firstName" placeholder="name" />
+            <Field name="lastName" placeholder="surname" />
+            <Field name="email" placeholder="email" />
+            <Field name="password" placeholder="password" />
+            {/* {error && <p>{error.message}</p>} */}
+            <button type="submit">Sign In</button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 
-  handlerSubmit = e => {
-    e.preventDefault();
-    console.log(this.state);
-    try {
-      const result = SIGN_UP_SCHEMA.validateSync(this.state);
-      console.log(result);
-    } catch (e) {
-      this.setState({
-        error: e,
-      });
-    }
-  };
-
-  render() {
-    const { email, password, firstName, lastName, error } = this.state;
-    return (
-      <form className={styles.form} onSubmit={this.handlerSubmit}>
-        <input
-          type="text"
-          name="firstName"
-          value={firstName}
-          placeholder="name"
-          onChange={this.changeHandler}
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
-          placeholder="surname"
-          onChange={this.changeHandler}
-        />
-        <input
-          type="text"
-          name="email"
-          value={email}
-          placeholder="email"
-          onChange={this.changeHandler}
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="password"
-          onChange={this.changeHandler}
-        />
-        {error && <p>{error.message}</p>}
-        <button type="submit">Sign In</button>
-      </form>
-    );
-  }
-}
+export default SignUpForm;
