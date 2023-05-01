118cdd1eeb30cdc0ebb7bcc6561c1ccce47a2d2d
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-01 10:22:04 +0300

    refactor

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import styles from './SignInForm.module.css';
-import yup from 'yup';
+import * as yup from 'yup';
 
-const SIGN_IN_SCHEMA = yup.object({
+const SIGN_UP_SCHEMA = yup.object({
   email: yup.string().required(),
   password: yup.string().required(),
+  firstName: yup.string(),
+  lastName: yup.string(),
 });
 
-export default class SignInForm extends Component {
+export default class SignUpForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
+      firstName: '',
+      lastName: '',
     };
   }
 
@@ -28,19 +32,35 @@ export default class SignInForm extends Component {
   };
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, firstName, lastName } = this.state;
     return (
       <form className={styles.form} onSubmit={this.handlerSubmit}>
         <input
           type="text"
           name="email"
           value={email}
+          placeholder=""
+          onChange={this.changeHandler}
+        />
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          placeholder=""
+          onChange={this.changeHandler}
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          placeholder=""
           onChange={this.changeHandler}
         />
         <input
           type="password"
           name="password"
           value={password}
+          placeholder=""
           onChange={this.changeHandler}
         />
         <button type="submit">Sign In</button>
