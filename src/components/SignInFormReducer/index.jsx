82a2b11004db42db59a63aa0014fb29094274e7a
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-05 10:55:17 +0300

    Add task (resolve?)

--- src/components/SignInFormReducer/index.jsx
+++ src/components/SignInFormReducer/index.jsx
@@ -1,60 +1,110 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'firstName':
+    case 'lastName':
+    case 'email':
+    case 'password':
+    case 'birthday':
+    case 'gender': {
+      return {
+        ...state,
+        [action.type]: action.value,
+      };
+    }
+    case 'form submitted': {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case 'reset': {
+      return {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        birthday: '',
+        gender: '',
+        isFetching: false,
+      };
+    }
+
+    case 'load': {
+      return {
+        ...action.data,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
 
 function SignInFormReducer() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [birthday, setBirthday] = useState('');
-  const [gender, setGender] = useState('');
+  const [state, dispatch] = useReducer(reducer, {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    birthday: '',
+    gender: '',
+    isFetching: false,
+  });
 
-  const changeHandler = ({ target: { name, value } }) => {
-    switch (name) {
-      case 'firstName': {
-        setFirstName(value);
-        break;
-      }
-      case 'lastName': {
-        setLastName(value);
-        break;
-      }
-      case 'email': {
-        setEmail(value);
-        break;
-      }
-      case 'password': {
-        setPassword(value);
-        break;
-      }
-      case 'birthday': {
-        setBirthday(value);
-        break;
-      }
+  const changeHandler = ({ target: { value, name } }) => {
+    const action = {
+      value,
+      type: name,
+    };
 
-      case 'gender': {
-        setGender(value);
-        break;
-      }
-
-      default: {
-      }
-    }
+    dispatch(action);
   };
 
   const submitHandler = e => {
     e.preventDefault();
-    console.log({
-      firstName,
-      lastName,
-      email,
-      password,
-      birthday,
-      gender,
+    dispatch({
+      type: 'form submitted',
     });
   };
 
+  const resetHandler = e => {
+    e.preventDefault();
+
+    dispatch({
+      type: 'reset',
+    });
+  };
+
+  const fetchData = e => {
+    e.preventDefault();
+
+    fetch('./userData.json')
+      .then(res => res.json())
+      .then(data =>
+        dispatch({
+          type: 'load',
+          data: { ...data },
+        })
+      );
+  };
+
+  const { firstName, lastName, email, password, birthday, gender, isFetching } =
+    state;
+
   return (
-    <form onSubmit={submitHandler}>
+    <form
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '300px',
+        height: '500px',
+        justifyContent: 'space-around',
+      }}
+    >
+      {isFetching && <div>Loading</div>}
       <input
         type="text"
         name="firstName"
@@ -67,12 +117,7 @@ function SignInFormReducer() {
         value={lastName}
         onChange={changeHandler}
       />
-      <input
-        type="email"
-        name="email"
-        value={email}
-        onChange={changeHandler}
-      />
+      <input type="email" name="email" value={email} onChange={changeHandler} />
       <input
         type="password"
         name="password"
@@ -80,7 +125,7 @@ function SignInFormReducer() {
         onChange={changeHandler}
       />
       <input
-        type="text"
+        type="date"
         name="birthday"
         value={birthday}
         onChange={changeHandler}
@@ -110,12 +155,15 @@ function SignInFormReducer() {
           type="radio"
           name="gender"
           value={gender}
-          onChange={setFirstName}
+          onChange={changeHandler}
         />
         Other
       </label>
-
-      <button>Submit</button>
+      <div>
+        <button onClick={submitHandler}>Submit</button>
+        <button onClick={resetHandler}>reset</button>
+        <button onClick={fetchData}>data</button>
+      </div>
     </form>
   );
 }
